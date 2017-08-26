23b9647d58be148fb22fb8ef68c2f0469f53c57f
wlsk0012 <wlsk0012@naver.com>
2017-08-26 11:14:26 +0900

    투두 수정모드

--- src/App.js
+++ src/App.js
@@ -1,53 +1,157 @@
 import React from 'react';
-
 import Header from './Header';
 import TodoList from './TodoList';
 import Footer from './Footer';
 
 class App extends React.Component {
     state = {
-        todos : [
-            {
-                text: '배고파',
-                id: 11111
-            }, {
-                text: '밥먹자',
-                id: 22222
-            }, {
-                text: '치킨에 맥주',
-                id: 12345
-            }, {
-                text: '삼겹살에 쏘주',
-                id: 123567
+        todos:[
+           {text:'배고파',
+            id:111
+            },
+             {text:'배고파1',
+            id:121
+            },
+             {text:'배고파',
+            id:113
+            },
+             {text:'배고파',
+            id:114
             }
-        ]
+        ],
+        editindgId: null
     };
 
+    //todo추가
     addTodo = text => {
+        //직접 setstate하지말것
+        // const newTodos = [... this.state.todos, text]; //spread operator
         this.setState({
-            todos: [...this.state.todos, {
-                text,
-                id: Date.now()
-            }]
+            todos : [... this.state.todos, text]
+        })
+    }
+
+    deleteTodo = index =>{
+        const newTodos =[... this.state.todos];
+        const targetIndex = newTodos.finding
+        import React from 'react';
+import Header from './Header';
+import TodoList from './TodoList';
+import Footer from './Footer';
+
+class App extends React.Component {
+    state = {
+        todos:[
+           {text:'배고파',
+            id:111
+            },
+             {text:'배고파1',
+            id:121
+            },
+             {text:'배고파',
+            id:113
+            },
+             {text:'배고파',
+            id:114
+            }
+        ],
+        editindgId: null
+    };
+
+    //todo추가
+    addTodo = text => {
+        //직접 setstate하지말것
+        // const newTodos = [... this.state.todos, text]; //spread operator
+        this.setState({
+            todos : [... this.state.todos, text]
+        })
+    }
+
+    deleteTodo = index =>{
+        const newTodos =[... this.state.todos];
+        const targetIndex = newTodos.findIndex(v => v.id === id);
+        newTodos.splice(index,1);
+        this.setState({
+            todos: newTodos
+        })
+    }
+
+    startEdit = id =>{
+        this.setState({
+            editingId:id
         });
     }
 
-    deleteTodo = id => {
-        const newTodos = [...this.state.todos];
+    saveTodo = (id,newText) =>{
+        const newTodos =[... this.state.todos];
         const targetIndex = newTodos.findIndex(v => v.id === id);
-        newTodos.splice(targetIndex, 1);
+
+        // =>(x) state 내부를 직접 바꾸는 결과를 초래되므로 지양하자
+        newTodos[targetIndex] = Object.assign({},newTodos[targetIndex],{
+            text: newText
+        })
         this.setState({
-            todos: newTodos
-        });
+            todos: newTodos,
+            editingId:null
+        })
     }
+
+    cancelEdit =() =>{
+        this.setState({
+            editingId: null
+        })
+    }
+
 
     render() {
         return (
             <div className="todo-app">
-                <Header addTodo={this.addTodo}/>
-                <TodoList
-                    todos={this.state.todos}
-                    deleteTodo={this.deleteTodo}
+                <Header addTodo={this.addTodo} />
+                <TodoList 
+                    todos={this.state.todos} 
+                    deleteTdo={this.deletetodo}
+                    startEdit={this.startEdit}
+                    editingId={this.state.editingId}
+                    saveTodo={this.saveTodo}
+                />
+                <Footer />
+            </div>
+        );
+    }
+}
+
+export default App;
+
+        this.setState({
+            todos: newTodos
+        })
+    }
+
+    startEdit = id =>{
+        this.setState({
+            editingId:id
+        });
+    }
+
+    saveTodo = (id,newText) =>{
+        const newTodos =[... this.state.todos];
+        newTodos.splice(index,1);
+        this.setState({
+            todos: newTodos
+        })
+    }
+
+
+    render() {
+        return (
+            <div className="todo-app">
+                <Header addTodo={this.addTodo} />
+                <TodoList 
+                    todos={this.state.todos} 
+                    deleteTdo={this.deletetodo}
+                    startEdit={this.startEdit}
+                    editingId={this.state.editingId}
+                    cancelEdit={this.cancelEdit}
                 />
                 <Footer />
             </div>
--- src/TodoList.js
+++ src/TodoList.js
@@ -3,6 +3,15 @@ import Todo from './Todo';
 
 class TodoList extends React.Component {
     render() {
+    	const{
+    		todos,
+    		deleteTodo,
+    		startEdit,
+    		editgindId,
+    		saveTodo,
+    		cancelEdit
+    	} = this.props;
+
         return (
             <div className="todo-app__main">
                 <ul className="todo-list">
@@ -10,7 +19,11 @@ class TodoList extends React.Component {
                         <Todo
                             key={`todo#${v.id}`}
                             text={v.text}
-                            deleteTodo={() => this.props.deleteTodo(v.id)}
+                            deleteTodo={() => deleteTodo(v.id)}
+                            startEdit={() => startEdit(v.id)}
+                            isEditing = {editgindId===v.id}
+                            saveTodo={text => saveTodo(v.id, text)}
+                            cancelEdit={cancelEdit}
                         />
                     ))}
                 </ul>
